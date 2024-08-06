cbc27b8acb5db7ef7566e4e8e00bd268df2a0df7
Naim <naimelhabbas@gmail.com>
2024-08-06 06:31:33 +0200

    Ajout de la modale

--- FrontEnd/AjouterWorks.js
+++ FrontEnd/AjouterWorks.js
@@ -46,10 +46,23 @@ async function testerSessionStorage(){
 
     
 
-    ajouterCategories()
+    if (sessionStorage.getItem('auth token')) {
+        
+        ajouterLogOut()
 
-    filterCategories()
+        afficherModeEdition()
+
+        lancerLaModale()
+        
+    }
+    else {
+
+            ajouterCategories()
+
+            filterCategories()
             
+    
+    }
     
     
 }
@@ -111,3 +124,364 @@ function filterCategories() {
 
 }
 
+/******************* afficher Logout au lieu de Login, le header: "mode édition" et l'encart modifier ******************/
+
+function ajouterLogOut() {
+    const login = document.getElementById("login")
+
+    login.textContent = "logout"
+
+    login.href = ''
+
+    login.addEventListener("click", ()=>{
+        sessionStorage.clear()
+        location.reload()
+    })
+}
+
+function afficherModeEdition() {
+    const modeEdition = document.querySelector(".modeEdition")
+    modeEdition.classList.remove("displayNone")
+    modeEdition.classList.add("displayFlex")
+}
+
+function lancerLaModale(){
+
+    ajouterEncartModifier()
+
+    ecouterEncartModifier()
+}
+
+function ajouterEncartModifier() {
+    
+    document.querySelector(".conteneurh2Modifier").innerHTML += `
+    <div class="encartModifier">
+    	<svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg" class="penToSquare black">
+		<path d="M14.0229 2.18576L14.3939 2.55679C14.6821 2.84503 14.6821 3.31113 14.3939 3.5963L13.5016 4.49169L12.0879 3.07808L12.9803 2.18576C13.2685 1.89751 13.7346 1.89751 14.0198 2.18576H14.0229ZM6.93332 8.23578L11.0484 4.11759L12.4621 5.53121L8.34387 9.64633C8.25494 9.73525 8.14455 9.79964 8.02496 9.83337L6.23111 10.3455L6.7432 8.55162C6.77693 8.43203 6.84133 8.32164 6.93025 8.23271L6.93332 8.23578ZM11.9408 1.14625L5.89074 7.1932C5.62397 7.45998 5.43078 7.78808 5.32959 8.14685L4.4526 11.2133C4.379 11.4708 4.44953 11.7468 4.63965 11.9369C4.82977 12.127 5.10574 12.1976 5.36332 12.124L8.42973 11.247C8.79156 11.1427 9.11967 10.9495 9.38338 10.6858L15.4334 4.63888C16.2951 3.77722 16.2951 2.37894 15.4334 1.51728L15.0624 1.14625C14.2007 0.284585 12.8024 0.284585 11.9408 1.14625ZM3.19844 2.34214C1.70816 2.34214 0.5 3.55031 0.5 5.04058V13.3812C0.5 14.8715 1.70816 16.0796 3.19844 16.0796H11.5391C13.0293 16.0796 14.2375 14.8715 14.2375 13.3812V9.94683C14.2375 9.539 13.9094 9.21089 13.5016 9.21089C13.0937 9.21089 12.7656 9.539 12.7656 9.94683V13.3812C12.7656 14.0589 12.2167 14.6078 11.5391 14.6078H3.19844C2.52076 14.6078 1.97188 14.0589 1.97188 13.3812V5.04058C1.97188 4.36291 2.52076 3.81402 3.19844 3.81402H6.63281C7.04065 3.81402 7.36875 3.48591 7.36875 3.07808C7.36875 2.67025 7.04065 2.34214 6.63281 2.34214H3.19844Z" fill="white"/>
+		</svg>
+    <p>modifier</p>
+    </div>
+    `
+}
+
+/******************* créer la modale si l'on clique sur "modifier" ******************/
+
+function ecouterEncartModifier() {
+
+    document.querySelector(".encartModifier").addEventListener("click", ()=> {
+
+        if (premiereModale==0) {
+
+            créerLaModale()
+
+            modaleAffichage1()
+
+            premiereModale++
+
+            return premiereModale
+
+        }
+        else {
+
+            modaleHtml.setAttribute("aria-hidden","false")
+            
+        }
+    })
+}
+
+function créerLaModale() {
+
+    modaleHtml.setAttribute("class","modale")
+    modaleHtml.setAttribute("aria-modal","true")
+    modaleHtml.setAttribute("aria-hidden","false")
+
+}
+
+/******************* affichage 1 de la modale ******************/
+
+function modaleAffichage1() {
+
+    modaleHtml.innerHTML = `
+        <div class="modal-wrapper">
+        <div class="arrowXmark">
+        ${svgArrow}
+        ${svgX}
+        </div>
+        <h3 class="titreWrapper">Galerie photo</h3>
+        <div class="photosContainer"></div>
+        <div class="categories ajouterPhoto mettreEnVert" id="AjouterPhoto">Ajouter une photo</div>       
+        </div>
+    `
+
+    ajouterWorksModale()
+
+    ecouterAffichage1()
+
+    ecouterSuppressionWorks()
+}
+
+async function ajouterWorksModale() {
+
+    const photosContainer = document.querySelector(".photosContainer")
+
+    photosContainer.innerHTML =''
+
+    for (const i of works) {
+
+        const workId = i.id
+
+        photosContainer.innerHTML += 
+            `
+            <div class="containerPhotoBin" id="box-${workId}">
+            <img src="${i.imageUrl}" class="imgModale">
+            <div class="binContainer" id="${workId}">
+            ${svgBin}
+            </div>
+            </div>
+            `
+    }
+}
+
+function ecouterAffichage1() {
+    
+    document.querySelector(".xmark").addEventListener("click", ()=> {
+        modaleHtml.setAttribute("aria-hidden","true")
+    })
+
+    document.getElementById("AjouterPhoto").addEventListener("click", ()=> {
+        modaleAffichage2()
+    })
+}
+
+/******************* supprimer les travaux dans l'affichage 1 et réinitialiser l'affichage de la gallerie et de la modale ******************/
+
+function ecouterSuppressionWorks() {
+    document.querySelectorAll(".binContainer").forEach(container =>{
+        container.addEventListener("click", async()=>{
+
+            await removeWork(container.id++)
+
+            ajouterWorksGallerie()
+
+            modaleAffichage1()
+        })
+    })
+}
+
+async function removeWork(element){
+    await fetch(`http://localhost:5678/api/works/${element}`, {
+        method:'DELETE',
+        headers:{
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${sessionStorage.getItem('auth token')}`
+    }
+    })
+
+    works = await setTableauWorks()
+}
+
+/******************* affichage 2 de la modale ******************/
+
+function modaleAffichage2() {
+
+    modaleHtml.innerHTML = `
+    <div class="modal-wrapper">
+
+    <div class="arrowXmark">
+
+        <svg width="21" height="18" viewBox="0 0 21 18" fill="none" xmlns="http://www.w3.org/2000/svg" class="arrow">
+        <path d="M0.439478 7.94458C-0.146493 8.53055 -0.146493 9.48217 0.439478 10.0681L7.9399 17.5686C8.52587 18.1545 9.47748 18.1545 10.0635 17.5686C10.6494 16.9826 10.6494 16.031 10.0635 15.445L5.11786 10.5041H19.4999C20.3297 10.5041 21 9.83375 21 9.00402C21 8.17428 20.3297 7.50393 19.4999 7.50393H5.12255L10.0588 2.56303C10.6447 1.97706 10.6447 1.02545 10.0588 0.439478C9.47279 -0.146493 8.52118 -0.146493 7.93521 0.439478L0.43479 7.9399L0.439478 7.94458Z" fill="black"/>
+        </svg>
+        ${svgX}
+    
+    </div>
+
+    <h3 class="titreWrapper">Ajout photo</h3>
+
+    <form action="#" method="post" class="menuAjouter">
+
+        <div class="zoneInput">
+
+            ${svgImg}
+            <label for="inputFile" id="labelInputFile" class="inputFile zoneInputBeforePreview" >+ Ajouter photo</label>
+            <input type="file" class="inputFile zoneInputBeforePreview" id="inputFile" accept="image/jpeg, image/png">
+            <span class="zoneInputBeforePreview">jpg, png : 4mo max</span>
+
+        </div>
+
+        <label for="Titre">Titre</label>
+        <input type="text" id="Titre">
+
+        <label for="Categorie">Catégorie</label>
+        <select id="Categorie">
+        </select>
+
+        <div  class="messageErreur"></div>
+
+    </form>
+
+    <div id="validerAjout" class="ajouterPhoto mettreEnGris">Valider</div>
+    </div>
+    
+    `
+    messageErreur = document.querySelector(".messageErreur")
+
+    ajouterOptionsCategories() /* met les options de catégories dans un menu déroulant */
+
+    vérifierTailleImage() /* vérifie que l'image entrée n'excede pas 4mo */
+
+    ecouterInputFile() /* vérifie si une image est entrée et ouvre sa prévisualisation */
+
+    ecouterTitre() /* vérifie si un titre est entré */
+
+    ecouterAjoutWorks() /* Vérifie si le formulaire est complet avant de poster */
+    
+    ecouterAffichage2() /* rend la flèche et la croix cliquables */
+}
+
+async function ajouterOptionsCategories() {
+
+    const categorieMenu = document.getElementById("Categorie")
+
+    for (const i of categories) {
+
+        const option = document.createElement('option')
+
+        option.value = i.id
+
+        option.textContent = i.name
+
+        categorieMenu.appendChild(option)
+
+    }
+}
+
+function vérifierTailleImage() {
+    const image = document.getElementById("inputFile")
+    image.addEventListener('change', ()=>{
+        const file = image.files[0]
+        const maxSize = 4 * 1024 * 1024
+
+        if (file && file.size > maxSize) {
+            messageErreur.innerHTML = "Fichier trop volumineux"
+            image.value = ""
+        }
+    })
+}
+
+function ecouterInputFile() {
+
+    const zoneInput = document.querySelector(".zoneInput")
+
+    const boutonFiles = document.getElementById("inputFile")
+
+
+    boutonFiles.addEventListener("change", ()=>{
+
+        testerFormComplete()
+
+        if (boutonFiles.files[0]) {
+
+            document.querySelectorAll(".zoneInputBeforePreview").forEach(element => {
+                element.classList.add("displayNone");
+            })
+
+            const img = document.createElement('img')
+
+            img.setAttribute('id', "imagePreview")
+
+            zoneInput.appendChild(img)
+
+            const url = URL.createObjectURL(boutonFiles.files[0])
+
+            document.getElementById("imagePreview").setAttribute('src', url)
+        }
+    })
+}
+
+function ecouterTitre() {
+    document.getElementById("Titre").addEventListener("change", ()=>{
+        testerFormComplete()
+    })
+}
+
+function ecouterAjoutWorks() {
+
+    const btnValider = document.getElementById("validerAjout")
+
+    btnValider.addEventListener("click", async()=>{
+
+        inputFile = document.getElementById("inputFile").files[0]
+        
+        Titre = document.getElementById("Titre").value
+        
+        const categorie = document.getElementById("Categorie").value
+
+        if (inputFile && Titre) {
+
+            await posterImage(inputFile, Titre, categorie)
+
+            ajouterWorksGallerie()
+
+            modaleAffichage2()
+
+            messageErreur.innerHTML = "Votre image à été ajoutée"
+
+            messageErreur.classList.add("vert")
+
+
+        }
+        else if (inputFile){
+            messageErreur.innerHTML = "Veuillez renseigner un titre"
+        }
+        else if (Titre) {
+            messageErreur.innerHTML = "Veuillez entrer une image"
+        }
+        else {
+            messageErreur.innerHTML = "Veuillez entrer un titre et une image"
+        }
+
+    })
+}
+
+function ecouterAffichage2() {
+    document.querySelector(".xmark").addEventListener("click", ()=> {
+        modaleHtml.setAttribute("aria-hidden","true")
+    })
+
+    document.querySelector(".arrow").addEventListener("click", ()=>{
+        modaleAffichage1()
+    })
+}
+
+ async function posterImage(inputFile, Titre, categorie) {
+
+    const formData = new FormData()
+
+    formData.append('title', Titre)
+    formData.append('category', categorie)
+    formData.append('image', inputFile)
+
+    await fetch(urlWorks, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${sessionStorage.getItem('auth token')}`
+        },
+        body: formData
+    })
+
+    works = await setTableauWorks()
+
+}
+
+function testerFormComplete() {
+
+    inputFile = document.getElementById("inputFile").files[0]
+        
+    Titre = document.getElementById("Titre").value
+
+    if (inputFile && Titre) {
+        document.getElementById("validerAjout").classList.add("mettreEnVert")
+        document.getElementById("validerAjout").classList.add("categories")
+    }
+}
